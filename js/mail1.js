e8d1349923c2044ee9006fd5506a39765f83d32c
LapminerYT <96932774+LapminerYT@users.noreply.github.com>
2023-06-28 09:48:27 +0700

    Update mail1.js

--- js/mail1.js
+++ js/mail1.js
@@ -22,7 +22,7 @@ firebase.initializeApp(firebaseConfig);
 
 // reference your database
 
-var contactFormDB = firebase.database().ref("editpro");
+var contactFormDB = firebase.database().ref("contactForm");
 
 document.getElementById("editpro").addEventListener("submit", submitForm);
 
